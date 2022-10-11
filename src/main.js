924bb212a1a1362c3a1e9fb4f9b743f535a7bc62
krisnaalipp <payman601@gmail.com>
2022-10-11 20:54:35 +0700

    Working on login page

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/assets/styles.css";
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
 app.use(router);
-
+app.use(pinia);
 app.mount("#app");
---
+++ src/views/DashboardCompany.vue
@@ -0,0 +1,46 @@
+<script>
+import NavBar from "../components/NavBar.vue";
+export default {
+  name: "Dashboard",
+  components: { NavBar },
+};
+</script>
+
+<template>
+  <NavBar />
+  <div class="row dashboard-cards">
+    <h2 class="d-flex justify-content-center m-5 text-black">Managers</h2>
+    <div class="card col-md-4">
+      <div class="card-title">
+        <h2>Manager Name</h2>
+        <div class="d-flex justify-content-end align-items-center">
+          <h3 class="bi bi-fire icon"></h3>
+        </div>
+        <medium>Manager Role</medium><br />
+        <small>Total Subordinate</small>
+      </div>
+    </div>
+    <div class="card col-md-4">
+      <div class="card-title">
+        <h2>Manager Name</h2>
+        <div class="d-flex justify-content-end align-items-center">
+          <h3 class="bi bi-fire icon"></h3>
+        </div>
+        <medium>Manager Role</medium><br />
+        <small>Total Subordinate</small>
+      </div>
+    </div>
+    <div class="card col-md-4">
+      <div class="card-title">
+        <h2>Manager Name</h2>
+        <div class="d-flex justify-content-end align-items-center">
+          <h3 class="bi bi-fire icon"></h3>
+        </div>
+        <medium>Manager Role</medium><br />
+        <small>Total Subordinate</small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
