1189b589b912b86c908497a6b6c64ed5cf58808b
Farfour07 <dimassds07@gmail.com>
2022-10-12 11:58:05 +0700

    update navbar

---
+++ client/src/views/ChatroomView.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "ChatroomView",
+};
+</script>
+
+<template>
+  <div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label"
+      >Example textarea</label
+    >
+    <textarea
+      class="form-control"
+      id="exampleFormControlTextarea1"
+      rows="3"
+    ></textarea>
+  </div>
+
+  <ul id="messages"></ul>
+  <form id="form" action="">
+    <input id="input" autocomplete="off" /><button>Send</button>
+  </form>
+</template>
