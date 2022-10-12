c05bb9faea7030de7a783339ec11afa14e187b39
Farfour07 <dimassds07@gmail.com>
2022-10-12 14:17:48 +0700

    add router link to navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -7,46 +7,47 @@ export default {
 <template>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <router-link to="/" class="navbar-brand">Home</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Introduction
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Story</a></li>
+            <li><a class="dropdown-item" href="#">Characters</a></li>
+            <li><a class="dropdown-item" href="#">Summons</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <router-link to="/chatroom" class="nav-link" >Chat Room</router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Hall of Fame</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Media</a>
         </li>
       </ul>
       
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav" >
           <li class="nav-item" >
             <a class="nav-link" href="" >
                 Logout
             </a>
           </li>
-          <li class="nav-item" >
-            <!-- <router-link to="/login"> -->
+          <li class="nav-link" >
+            <router-link to="/login">
                 Have an account? Login now
-            <!-- </router-link> -->
+            </router-link>
           </li>
         </ul>
+      </div>
       
     </div>
   </div>
