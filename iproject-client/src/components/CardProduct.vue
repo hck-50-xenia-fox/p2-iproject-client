c67d541a6628f6dc917859dfa37bd4ec74d5a8b8
aryayudhazachri22 <aryayudhazachri09@gmail.com>
2022-10-12 17:12:29 +0700

    add payment

--- iproject-client/src/components/CardProduct.vue
+++ iproject-client/src/components/CardProduct.vue
@@ -1,17 +1,25 @@
 <script>
-    export default {
-        name: "CardProduct",
-        props: ["product"]
-    }
+import { mapActions } from "pinia";
+import { useProductStore } from "../stores/storeProduct";
+export default {
+  name: "CardProduct",
+  props: ["product"],
+  computed: {
+    ...mapActions(useProductStore, ["payments"]),
+  },
+};
 </script>
 
 <template>
-     <div class="col-3 mt-4" data-aos="fade-up">
+  <div class="col-3 mt-4">
     <div class="card">
-      <img :src="product.imgUrl" class="card-img-top" alt="..."/>
+      <img :src="product.imgUrl" class="card-img-top" alt="..." />
       <div class="card-body">
         <h6 class="card-title">{{ product.name }}</h6>
         <p>Rp.{{ product.price }}</p>
+        <button @click.prevent="this.payments()" id="pay-button" class="btn btn-primary">
+          Buy
+        </button>
       </div>
     </div>
   </div>
--- iproject-client/src/stores/storeProduct.js
+++ iproject-client/src/stores/storeProduct.js
@@ -5,7 +5,8 @@ let baseUrl = "http://localhost:3000/"
 export const useProductStore = defineStore("product", {
     state: () => {
         return{
-            dataProduct: []
+            dataProduct: [],
+            paymentResponse: {},
         }
     },
     actions:{
@@ -22,6 +23,30 @@ export const useProductStore = defineStore("product", {
             } catch (error) {
                 console.log(error)
             }
-        }
+        },
+        async payments() {
+            try {
+                let { data } = await axios({
+                    method: `POST`,
+                    url: baseUrl + `product/payment`,
+                    headers: {
+                        access_token: localStorage.getItem("access_token")
+                    }
+                })
+                console.log(data);
+
+                snap.pay(`${data.transactionToken}`, {
+                    onSuccess: async function (result) {
+                        console.log(result);
+                        this.paymentResponse = result
+                    },
+                    onPending: function (result) { console.log('pending'); console.log(result); },
+                    onError: function (result) { console.log('error'); console.log(result); },
+                    onClose: function () { console.log('customer closed the popup without finishing the payment'); }
+                })
+            } catch (err) {
+                console.log(err);
+            }
+        },
     }
 })
