8fd23971d4c893f7384bc0a40747fcd41e415b1e
MuhamadAdam <dam.n8910@gmail.com>
2022-10-12 11:44:37 +0700

    template for vue

---
+++ I-Project/src/stores/index.js
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3000'
+
+export const useIprojectStore = defineStore('Iproject',{
+    state:()=>({
+
+    }),
+    actions:{},
+
+})
