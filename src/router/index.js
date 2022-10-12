f03ee5e11257e6b455120af3070e2cfc29dd5381
adibwafi <adibwafi@gmail.com>
2022-10-12 23:15:00 +0700

    done add about us

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import RentMotorcycle from '../pages/RentMotorcycle.vue'
+import AboutUs from '../pages/AboutUs.vue'
 
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/rental',
       name: 'rent',
       component: RentMotorcycle
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutUs
     },
   ]
 })
