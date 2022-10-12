f80d385256777df0ae844424e1638088a59651df
Shania <shania1faza@gmail.com>
2022-10-13 00:38:01 +0700

    setting up templates

---
+++ client/src/components/FooterBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <footer class="footer">
+    <div class="page-up">
+        <a href="#" id="scrollToTopButton"><span class="arrow_carrot-up"></span></a>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="footer__logo">
+                    <a href="./index.html"><img src="../assets/img/logo.png" alt=""></a>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="footer__nav">
+                    <ul>
+                        <li class="active"><a href="./index.html">Homepage</a></li>
+                        <li><a href="./categories.html">Categories</a></li>
+                        <li><a href="./blog.html">Our Blog</a></li>
+                        <li><a href="#">Contacts</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                  Copyright &copy;2022 All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by Colorlib x Shania
+                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+
+              </div>
+          </div>
+      </div>
+  </footer>
+  <!-- Footer Section End -->
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
---
+++ client/src/components/MainSection.vue
@@ -0,0 +1,118 @@
+<template>
+    <section class="product spad">
+      <div class="container">
+        <div class="row">
+
+            <!-- LEFT SECTION -->
+          <div class="col-lg-8">
+            <div class="trending__product">
+              <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-8">
+                  <div class="section-title">
+                    <h4>Trending Now</h4>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4">
+                  <div class="btn__all">
+                    <a href="#" class="primary-btn"
+                      >View All <span class="arrow_right"></span
+                    ></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                  <div class="product__item">
+                    <div
+                      class="product__item__pic set-bg"
+                      data-setbg="img/trending/trend-1.jpg"
+                    >
+                      <div class="ep">18 / 18</div>
+                      <div class="comment">
+                        <i class="fa fa-comments"></i> 11
+                      </div>
+                      <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                    </div>
+                    <div class="product__item__text">
+                      <ul>
+                        <li>Active</li>
+                        <li>Movie</li>
+                      </ul>
+                      <h5>
+                        <a href="#">The Seven Deadly Sins: Wrath of the Gods</a>
+                      </h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+            <!-- LEFT SECTION END -->
+
+            <!-- RIGHT SECTION -->
+          <div class="col-lg-4 col-md-6 col-sm-8">
+            <div class="product__sidebar">
+              <div class="product__sidebar__view">
+                <div class="section-title">
+                  <h5>Top Views</h5>
+                </div>
+                <ul class="filter__controls">
+                  <li class="active" data-filter="*">Day</li>
+                  <li data-filter=".week">Week</li>
+                  <li data-filter=".month">Month</li>
+                  <li data-filter=".years">Years</li>
+                </ul>
+                <div class="filter__gallery">
+                  <div
+                    class="product__sidebar__view__item set-bg mix day years"
+                    data-setbg="img/sidebar/tv-1.jpg"
+                  >
+                    <div class="ep">18 / ?</div>
+                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                    <h5><a href="#">Boruto: Naruto next generations</a></h5>
+                  </div>
+                </div>
+              </div>
+
+              <!-- RIGHT SECTION 2 -->
+              <div class="product__sidebar__comment">
+                <div class="section-title">
+                  <h5>New Comment</h5>
+                </div>
+                <div class="product__sidebar__comment__item">
+                  <div class="product__sidebar__comment__item__pic">
+                    <img src="../assets/img/sidebar/comment-1.jpg" alt="" />
+                  </div>
+                  <div class="product__sidebar__comment__item__text">
+                    <ul>
+                      <li>Active</li>
+                      <li>Movie</li>
+                    </ul>
+                    <h5>
+                      <a href="#">The Seven Deadly Sins: Wrath of the Gods</a>
+                    </h5>
+                    <span><i class="fa fa-eye"></i> 19.141 Viewes</span>
+                  </div>
+                </div>
+              </div>
+              <!-- RIGHT SECTION 2 END -->
+
+            </div>
+          </div>
+            <!-- RIGHT SECTION END -->
+
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
