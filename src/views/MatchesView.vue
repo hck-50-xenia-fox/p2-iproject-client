a6513bae740e54bb9e2955a633a59c80bd7a9199
alwanWicaksono <alwanwicaksono1@gmail.com>
2022-10-13 09:47:21 +0700

    matches page

---
+++ src/views/MatchesView.vue
@@ -0,0 +1,24 @@
+<script>
+import { mapActions, mapState } from 'pinia';
+import { useCounterStore } from '../stores/counter';
+import ListNews from '../components/ListNews.vue';
+  export default{
+    components: {ListNews},
+    methods: {
+      ...mapActions(useCounterStore, ["showMatches"])
+    },
+    computed: {
+      ...mapState(useCounterStore, ["dataMatches"])
+    },
+    created() {
+      this.showMatches()
+    }
+  }
+</script>
+
+<template>
+  <h1>Next Matches</h1>
+  <main>
+    <ListNews v-for="data in dataMatches" :key=data.id :data=data pageName="matches"/>
+  </main>
+</template>
