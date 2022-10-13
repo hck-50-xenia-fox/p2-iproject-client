e6b8e420d99521799bdee0a792c591c5e61fbab3
gioiaswndr <annashkagioia24@gmail.com>
2022-10-13 10:53:34 +0700

    done homeview

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,3 +1,58 @@
-<script></script>
+<script>
+import StreamerCard from "../components/StreamerCard.vue";
+import VideoCard from "../components/VideoCard.vue";
+import Table from "../components/Table.vue";
+import { mapActions, mapState } from "pinia";
+import { useMasterStore } from "../stores/masterStore";
+import Puzzle from "../components/Puzzle.vue";
 
-<template></template>
+export default {
+  name: "HomeView",
+  components: { StreamerCard, VideoCard, Table, Puzzle },
+  methods: {
+    ...mapActions(useMasterStore, [
+      "getTopPlayers",
+      "getStreamer",
+      "getPuzzle",
+      "getYoutube",
+    ]),
+  },
+  computed: {
+    ...mapState(useMasterStore, [
+      "topPlayer",
+      "dataStreamer",
+      "dataPuzzle",
+      "dataUtube",
+    ]),
+  },
+  async created() {
+    await this.getTopPlayers();
+    await this.getStreamer();
+    await this.getPuzzle();
+    await this.getYoutube();
+  },
+};
+</script>
+
+<template>
+  <!-- <pre>{{ topPlayer }}</pre> -->
+
+  <Table />
+
+  <!-- <pre>{{ dataStreamer }}</pre> -->
+  <div class="grid grid-cols-3 gap-3 justify-items-center p-14">
+    <VideoCard v-for="streamer in dataStreamer" :streamer="streamer" />
+  </div>
+  <!-- BUAT LIST VIDEO -->
+
+  <div class="grid grid-cols-2 gap-3 justify-items-center mt-7 p-14">
+    <!-- <pre>{{ dataPuzzle }}</pre> -->
+    <Puzzle v-for="puzzle in dataPuzzle" :puzzle="puzzle" />
+  </div>
+
+  <!-- <pre>{{ dataUtube }}</pre> -->
+  <div class="grid grid-cols-3 gap-3 justify-items-center px-9 mb-10">
+    <StreamerCard v-for="video in dataUtube" :video="video" />
+  </div>
+  <!-- BUAT LIST STREAMER -->
+</template>
